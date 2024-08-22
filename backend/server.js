5796392007955903a3793fb98297b712b841d237
Keher <prabalpsingh7115@gmail.com>
2024-08-22 14:45:12 +0530

    update cors

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ import GetParagraph from "./functions/getParagraph.js";
 
 
 const corsOptions = {
-    origin: ['http://localhost:5173','https://panda-type.netlify.app'],
+    origin: process.env.TRUSTED_DOMAINS.split(','),
     optionsSuccessStatus: 200,
     credentials:true
 };
@@ -31,7 +31,7 @@ const server = createServer(app)
 
 const io = new Server(server, {
     cors: {
-        origin: ['http://localhost:5173', 'https://panda-type.netlify.app'],
+        origin: process.env.TRUSTED_DOMAINS.split(','),
         methods: ["GET", "POST"],
         credentials: true
     }
